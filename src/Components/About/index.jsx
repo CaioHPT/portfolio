ae91d4ec1e767ecd8ec9fc514343b76f65d411f9
CaioHPT <caio.h.teixeira@gmail.com>
2022-03-07 10:40:00 -0300

    alteração de texto

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -12,16 +12,18 @@ const About = () => {
                 <div className="aboutText">
                     <h4>Caio Teixeira - Desenvolvedor Full Stack</h4>
                     <p>
-                        Olá, eu sou o Caio, tenho 18 anos e sou programador Full Stack, 
-                        atualmente trabalho para a Recode como Monitor do programa RecodePro. 
-                        Sou muito curioso sobre assuntos que envolvem o mundo tech de uma maneira geral 
-                        e sigo sempre estudando e me atualizando, especialmente em programação. 
-                        Eu estudo programação desdo meus 16 anos e neste período já tive alguns aprendizados bem marcantes: 
-                        1. É uma área desafiadora e eu gosto de desafios, 
-                        um deles foi meu primeiro projeto completo com front end e back end ligados, que foi meu tcc. 
-                        2. Aprendemos coisas novas o tempo todo e isso me motiva muito, adoro ser provocado a aprender algo novo. 
-                        3. A maioria dos aprendizados são construídos nas trocas de  informações e compartilhamento de conhecimentos com outras pessoas e, para mim,  
-                        trabalhar e estudar em equipe/times tem sido um dos pontos mais relevantes da minha carreira.
+                    Olá, eu sou o Caio, tenho 18 anos e sou programador Full Stack, atualmente 
+                    trabalho para a Recode como Monitor do programa RecodePro. 
+                    Sou muito curioso sobre assuntos que envolvem o mundo tech de uma maneira geral 
+                    e sigo sempre estudando e me atualizando, especialmente em programação. 
+                    Eu estudo programação desde meus 16 anos e neste período seleciono esses 3 pontos como os maiores aprendizados até aqui: 
+                    1. É uma área desafiadora e eu gosto disso, um desafio recente foi meu primeiro projeto completo 
+                    envolvendo front end e back end ligados, apresentado como TCC. 
+                    2. Aprendo coisas novas o tempo todo e isso me motiva muito, 
+                    adoro ser provocado a aprender algo novo. 
+                    3. A maioria dos aprendizados são construídos nas trocas de informações e compartilhamento de 
+                    conhecimento com outras pessoas e, 
+                    para mim, trabalhar e estudar em equipe/times tem sido um dos pontos mais relevantes da minha carreira.
                     </p>
                 </div>
             </section>
